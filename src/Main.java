fb1375703a5546b357f3dcdd7511bcc0f2d1616f
Izaias Machado <izaiasmachado.dev@gmail.com>
2022-07-12 15:46:54 -0300

    fix: conflict resolution

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-
 		try {
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/Util.java
+++ src/Util.java
@@ -1,8 +1,5 @@
-package main;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
